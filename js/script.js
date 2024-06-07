125ae516d9f8a0be1b4cd824bc2a9b9711a58861
levmn <levimncontato@gmail.com>
2024-06-06 22:14:06 -0300

    Ajustando redirecionamento de formulários com javascript

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-// redirecionamento da página de cadastro para o dashboard por js
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // adicionar qualquer lógica adicional, como validação de formulário
-    window.location.href = 'dashboard.html';
+document.addEventListener('DOMContentLoaded', function () {
+    // redirecionamento da página de login para o dashboard por js
+    document.getElementById('loginForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // lógica adicional, como validação de formulário
+        window.location.href = 'dashboard.html';
+    });
+
+    // redirecionamento da página de cadastro para o dashboard por js
+    document.getElementById('cadastroForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // lógica adicional, como validação de formulário
+        window.location.href = 'dashboard.html';
+    });
 });
