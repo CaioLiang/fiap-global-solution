b615711f588839bbc3d9c97c279bd0956b34074e
levmn <levimncontato@gmail.com>
2024-06-07 10:17:55 -0300

    Adicionando página de gerenciar despejos e ajustes de estilização

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     // redirecionamento da página de login para o dashboard por js
-    document.getElementById('loginForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-        // lógica adicional, como validação de formulário
-        window.location.href = 'dashboard.html';
-    });
+    var loginForm = document.getElementById('loginForm');
+    if (loginForm) {
+        loginForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            console.log('Login form submitted');
+            // lógica adicional, como validação de formulário
+            window.location.href = './dashboard.html';
+        });
+    } else {
+        console.log('Login form not found');
+    }
 
     // redirecionamento da página de cadastro para o dashboard por js
-    document.getElementById('cadastroForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-        // lógica adicional, como validação de formulário
-        window.location.href = 'dashboard.html';
-    });
+    var cadastroForm = document.getElementById('cadastroForm');
+    if (cadastroForm) {
+        cadastroForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            console.log('Cadastro form submitted');
+            // lógica adicional, como validação de formulário
+            window.location.href = './dashboard.html';
+        });
+    } else {
+        console.log('Cadastro form not found');
+    }
 });
